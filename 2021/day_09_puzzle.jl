114809ed9e9bf61182ec06c1b051b15b5bc64f51
labellca <labellca@videotron.ca>
2022-12-05 10:15:01 -0500

    2021 repo

--- 2021/day_09_puzzle.jl
+++ 2021/day_09_puzzle.jl
@@ -34,8 +34,7 @@ for x in 1:X
         location = data[x, y]
         neighbour_heights = neighbours(data, x, y, X, Y)
         
-        if sum(neighbour_heights .< location) == 0
-            println(location, " => ", neighbour_heights)
+        if sum(neighbour_heights .> location) == length(neighbour_heights)
             push!(risk_levels, location + 1)
         end
     end
@@ -44,3 +43,95 @@ end
 sum_risk_levels = sum(risk_levels)
 println("Part 1: What is the sum of the risk levels of all low points on your heightmap?")
 println(sum_risk_levels)
+
+### basin
+### all locations that eventually flow downward to a single low point
+### every low point is part of a basin
+### location height 9 is never in a basin
+### one location is part of a single basin
+### size: number of locations within the basin (including low point)
+function getHeight_up(data::Matrix{Int}, start_row::Int, col::Int)
+    tmp_up = []
+    for ri in start_row-1:-1:1
+        if data[ri, col] == 9
+            return tmp_up
+        else
+            push!(tmp_up, data[ri, col])
+        end 
+    end
+    return tmp_up
+end
+
+function getHeight_down(data::Matrix{Int}, start_row::Int, col::Int)
+    last_row = size(data)[1]
+
+    tmp_down = []
+    for ri in start_row+1:last_row
+        if data[ri, col] == 9
+            return tmp_down
+        else
+            push!(tmp_down, data[ri, col])
+        end 
+    end
+    return tmp_down
+end
+
+function getHeight_left(data::Matrix{Int}, start_col::Int, row::Int)
+    tmp_left = []
+    for ci in start_col:-1:1
+        if data[row, ci] == 9
+            return tmp_left
+        else
+            #println(data[row, ci])
+            push!(tmp_left, data[row, ci])
+            #append!(tmp_left, getHeight_up(data, row, ci))
+            #append!(tmp_left, getHeight_down(data, row, ci))
+        end 
+    end
+    return tmp_left
+end
+
+function getHeight_right(data::Matrix{Int}, start_col::Int, row::Int)
+    last_col = size(data)[2]
+
+    tmp_right = []
+    for ci in start_col+1:last_col
+        if data[row, ci] == 9
+            return tmp_right
+        else
+            push!(tmp_right, data[row, ci])
+            #append!(tmp_right, getHeight_up(data, row, ci))
+            #append!(tmp_right, getHeight_down(data, row, ci))
+        end 
+    end
+    return tmp_right
+end
+
+
+basins_size = []
+
+for x in 1:X
+    for y in 1:Y
+        location = data[x, y]
+        neighbour_heights = neighbours(data, x, y, X, Y)
+        
+        if sum(neighbour_heights .> location) == length(neighbour_heights)
+            tmp = []
+            append!(tmp, getHeight_left(data, y, x))
+            append!(tmp, getHeight_right(data, y, x))
+
+            push!(basins_size, length(tmp))
+        end
+    end
+end
+
+
+data = parseInput_Array(input)
+
+for x in 1:X
+    for y in 1:Y
+
+        
+
+    end
+end
